ccd2de325d4a1a6cef26b9978d29f55330bd76ee
khiemnguyen812 <ntkhiem21@clc.fitus.edu.vn>
2025-01-18 00:04:33 +0700

    first commit

---
+++ src/Acme.BookStore.Blazor.Client/Navigation/BookStoreMenuContributor.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading.Tasks;
+using Acme.BookStore.Localization;
+using Acme.BookStore.MultiTenancy;
+using Acme.BookStore.Permissions;
+using Microsoft.Extensions.Configuration;
+using Volo.Abp.Account.Localization;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.Identity.Blazor;
+using Volo.Abp.SettingManagement.Blazor.Menus;
+using Volo.Abp.TenantManagement.Blazor.Navigation;
+using Volo.Abp.UI.Navigation;
+using Volo.Abp.Users;
+
+namespace Acme.BookStore.Blazor.Client.Navigation;
+
+public class BookStoreMenuContributor : IMenuContributor
+{
+    private readonly IConfiguration _configuration;
+
+    public BookStoreMenuContributor(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task ConfigureMenuAsync(MenuConfigurationContext context)
+    {
+        if (context.Menu.Name == StandardMenus.Main)
+        {
+            await ConfigureMainMenuAsync(context);
+        }
+        else if (context.Menu.Name == StandardMenus.User)
+        {
+            await ConfigureUserMenuAsync(context);
+        }
+    }
+
+    private static async Task ConfigureMainMenuAsync(MenuConfigurationContext context)
+    {
+        var l = context.GetLocalizer<BookStoreResource>();
+
+        //Administration
+        var administration = context.Menu.GetAdministration();
+        administration.Order = 5;
+
+        context.Menu.AddItem(
+            new ApplicationMenuItem(
+                BookStoreMenus.Home,
+                l["Menu:Home"],
+                "/",
+                icon: "fas fa-home",
+                order: 1
+            )
+        );
+        if (MultiTenancyConsts.IsEnabled)
+        {
+            administration.SetSubItemOrder(TenantManagementMenuNames.GroupName, 1);
+        }
+        else
+        {
+            administration.TryRemoveMenuItem(TenantManagementMenuNames.GroupName);
+        }
+
+        context.Menu.AddItem(
+            new ApplicationMenuItem("BooksStore", l["Menu:BookStore"], icon: "fa fa-book").AddItem(
+                new ApplicationMenuItem("BooksStore.Books", l["Menu:Books"], url: "/books")
+            )
+        );
+
+        administration.SetSubItemOrder(IdentityMenuNames.GroupName, 2);
+        administration.SetSubItemOrder(SettingManagementMenus.GroupName, 3);
+    }
+
+    private async Task ConfigureUserMenuAsync(MenuConfigurationContext context)
+    {
+        var accountStringLocalizer = context.GetLocalizer<AccountResource>();
+        var authServerUrl = _configuration["AuthServer:Authority"] ?? "";
+
+        context.Menu.AddItem(
+            new ApplicationMenuItem(
+                "Account.Manage",
+                accountStringLocalizer["MyAccount"],
+                $"{authServerUrl.EnsureEndsWith('/')}Account/Manage",
+                icon: "fa fa-cog",
+                order: 1000,
+                target: "_blank"
+            ).RequireAuthenticated()
+        );
+
+        await Task.CompletedTask;
+    }
+}
---
+++ src/Acme.BookStore.Domain/BookStoreDataSeederContributor.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading.Tasks;
+using Acme.BookStore.Books;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace BookStore;
+
+public class BookStoreDataSeederContributor : IDataSeedContributor, ITransientDependency
+{
+    private readonly IRepository<Book, Guid> _bookRepository;
+
+    public BookStoreDataSeederContributor(IRepository<Book, Guid> bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+
+    public async Task SeedAsync(DataSeedContext context)
+    {
+        if (await _bookRepository.GetCountAsync() <= 0)
+        {
+            await _bookRepository.InsertAsync(
+                new Book
+                {
+                    Name = "1984",
+                    Type = BookType.Dystopia,
+                    PublishDate = new DateTime(1949, 6, 8),
+                    Price = 19.84f,
+                },
+                autoSave: true
+            );
+
+            await _bookRepository.InsertAsync(
+                new Book
+                {
+                    Name = "The Hitchhiker's Guide to the Galaxy",
+                    Type = BookType.ScienceFiction,
+                    PublishDate = new DateTime(1995, 9, 27),
+                    Price = 42.0f,
+                },
+                autoSave: true
+            );
+            await _bookRepository.InsertAsync(
+                new Book
+                {
+                    Name = "Brave New World",
+                    Type = BookType.Dystopia,
+                    PublishDate = new DateTime(1932, 8, 18),
+                    Price = 15.99f,
+                },
+                autoSave: true
+            );
+
+            await _bookRepository.InsertAsync(
+                new Book
+                {
+                    Name = "Fahrenheit 451",
+                    Type = BookType.Dystopia,
+                    PublishDate = new DateTime(1953, 10, 19),
+                    Price = 12.99f,
+                },
+                autoSave: true
+            );
+
+            await _bookRepository.InsertAsync(
+                new Book
+                {
+                    Name = "To Kill a Mockingbird",
+                    Type = BookType.Fiction,
+                    PublishDate = new DateTime(1960, 7, 11),
+                    Price = 18.99f,
+                },
+                autoSave: true
+            );
+
+            await _bookRepository.InsertAsync(
+                new Book
+                {
+                    Name = "The Great Gatsby",
+                    Type = BookType.Fiction,
+                    PublishDate = new DateTime(1925, 4, 10),
+                    Price = 10.99f,
+                },
+                autoSave: true
+            );
+
+            await _bookRepository.InsertAsync(
+                new Book
+                {
+                    Name = "Moby Dick",
+                    Type = BookType.Fiction,
+                    PublishDate = new DateTime(1851, 10, 18),
+                    Price = 22.50f,
+                },
+                autoSave: true
+            );
+        }
+    }
+}
